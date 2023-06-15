25079356e80576ed41fb37a668ca60f38e2dd016
Nathan Schneider <nsschneider1@gmail.com>
2023-06-15 14:17:55 -0300

    Fix generating products with global attributes

--- attributes-generator.php
+++ attributes-generator.php
@@ -5,8 +5,8 @@
  */
 
 
-const NUMBER_OF_ATTRIBUTES = 1;
-const NUMBER_OF_TERMS      = 5;
+const NUMBER_OF_PRODUCTS                 = 100;
+const NUMBER_OF_TERMS_PER_GLOBAL_ATTR    = 5;
 
 function register_admin_menu() {
 	$hook = add_management_page( 'Attributes Generator', 'Attributes Generator', 'install_plugins', 'attributesgenerator', 'render_admin_page' );
@@ -14,21 +14,20 @@ function register_admin_menu() {
 }
 
 function render_admin_page() {
-	$number_of_attributes = NUMBER_OF_ATTRIBUTES;
+	$number_of_products = NUMBER_OF_PRODUCTS;
 	?>
   <h1>Attributes Performance Test</h1>
   <form method="post">
 	<?php wp_nonce_field( 'generate', 'attributesgenerator_nonce' ); ?>
-	<?php submit_button( "Generate $number_of_attributes global attributes", 'primary', 'generate_global_attributes' ); ?>
-	<?php submit_button( "Generate $number_of_attributes products with local attributes", 'primary', 'generate_products_with_local_attributes' ); ?>
-	<?php submit_button( "Generate $number_of_attributes products with existing global attributes", 'primary', 'generate_products_with_existing_global_attributes' ); ?>
+	<?php submit_button( "Generate $number_of_products global attributes and products", 'primary', 'generate_global_attributes' ); ?>
+	<?php submit_button( "Generate $number_of_products products with local attributes", 'primary', 'generate_products_with_local_attributes' ); ?>
 	<?php submit_button( 'Delete all global attributes', 'primary', 'delete_all_global_attributes' ); ?>
 	</form>
 	<?php
 }
 
 function generate_global_attributes_and_products() {
-	for ( $i = 0; $i < NUMBER_OF_ATTRIBUTES; $i++ ) {
+	for ( $i = 0; $i < NUMBER_OF_PRODUCTS; $i++ ) {
 		$attribute_name = "Generated attribute $i";
 		$id             = wc_create_attribute( array( 'name' => $attribute_name ) );
 
@@ -52,30 +51,31 @@ function generate_global_attributes_and_products() {
 			)
 		);
 		$terms = array();
-		for ( $j = 0; $j < NUMBER_OF_TERMS; $j++ ) {
+		for ( $j = 0; $j < NUMBER_OF_TERMS_PER_GLOBAL_ATTR; $j++ ) {
 			$term_obj = wp_insert_term( "$attribute_name term $j", $taxonomy_name );
 			$terms[] = $term_obj['term_id'];
 		}
 
 		$attribute = new \WC_Product_Attribute();
 		$attribute->set_id( $id );
-		$attribute->set_name( $attribute_name );
+		$attribute->set_name( $taxonomy_name );
 		$attribute->set_options( $terms );
+		$attribute->set_visible( true );
+		$attribute->set_variation( true );
 		$product = new \WC_Product_Simple();
-		$product->set_name( 'Test Creating Product' );
+		$product->set_name( "Product with global attribute $i" );
 		$product->set_attributes(
 			array( $attribute )
 		);
 		$product->save();
 
-		// TODO find out why the attribute is not showing up
 	}
 }
 
 function generate_products_with_local_attributes() {
-	for ( $i = 0; $i < NUMBER_OF_ATTRIBUTES; $i++ ) {
+	for ( $i = 0; $i < NUMBER_OF_PRODUCTS; $i++ ) {
 		$product = new \WC_Product_Simple();
-		$product->set_name( "Test Creating Product $i" );
+		$product->set_name( "Product with local attributes $i" );
 		$attribute1 = new \WC_Product_Attribute();
 		$attribute1->set_name( 'attribute 1' );
 		$attribute1->set_options( array( 'option 1', 'option 2', 'option 3' ) );
@@ -106,34 +106,12 @@ function generate_products_with_local_attributes() {
 	}
 }
 
-function generate_products_with_existing_global_attributes() {
-	$attribute = wc_get_attribute( 7 );
-
-	$attribute_object = new WC_Product_Attribute();
-
-	$attribute_object->set_id( $attribute->id );
-	$attribute_object->set_name( $attribute->name );
-	$attribute_object->set_position( 0 );
-	// $attribute_object->set_options();
-
-	for ( $i = 0; $i < NUMBER_OF_ATTRIBUTES; $i++ ) {
-		$product = new \WC_Product_Simple();
-		$product->set_name( "Test Creating Product With Global Attributes $i" );
-		$product->set_attributes(
-			array( $attribute_object )
-		);
-		$product->save();
-	}
-}
-
 function process_page_submit() {
 	global $wpdb;
 	if ( ! empty( $_POST['generate_global_attributes'] ) ) {
 		generate_global_attributes_and_products();
 	} elseif ( ! empty( $_POST['generate_products_with_local_attributes'] ) ) {
 		generate_products_with_local_attributes();
-	} elseif ( ! empty( $_POST['generate_products_with_existing_global_attributes'] ) ) {
-		generate_products_with_existing_global_attributes();
 	} elseif ( ! empty( $_POST['delete_all_global_attributes'] ) ) {
 		$results = $wpdb->get_results( 'SELECT attribute_id FROM wp_woocommerce_attribute_taxonomies', ARRAY_N );
 		foreach ( $results as $result ) {
